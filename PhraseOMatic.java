bc54b8433d1f3cf1876d2480b8850138e25914d7
Joel Chester <joel.chester@gmail.com>
2017-11-21 18:00:47 -0500

    Update PhraseOMatic.java

--- PhraseOMatic.java
+++ PhraseOMatic.java
@@ -3,7 +3,7 @@ package com.daytonadaynePhrase;
 public class PhraseOMatic { 
 	public static void main (String[] args) {
 
-	// make three sets of words to choose from. Add your own! 
+	// make three sets of words to choose from. 
 	String[] wordListOne = {"24/7", "multi-Tier", "30,000 foot", "B-to-B", "win-win", "front-end", "web-based", "persvasive", "smart", "six-sigma", "critical-path", "dynamic"}; 
 
 
